d46fdea829e4303c7b5bae9d0c8a345528bd0dcf
e3 <eerwitt@gmail.com>
2015-11-04 14:33:42 -0800

    Added recursive solution.

--- fib.js
+++ fib.js
@@ -3,5 +3,18 @@
  * @param n {Integer} Number of levels deep from Fibonacci sequence.
  * @returns {Integer} Number from the Fibonacci sequence.
  */
-function fibo(n) {
+function fib(n) {
+  if (n < 2) {
+    return 1;
+  } else {
+    return fib(n - 1) + fib(n - 2);
+  }
 }
+
+// Go over first 10 elements of the Fibonacci sequence.
+for (var i = 0; i < 10; i++) {
+  console.log(fib(i));
+}
+
+// Should log:
+// 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
