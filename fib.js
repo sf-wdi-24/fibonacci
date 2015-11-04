d7053109417cbe9f2f401f6734ca42aac0012818
e3 <eerwitt@gmail.com>
2015-11-04 14:35:14 -0800

    Adding some extra help text.

--- fib.js
+++ fib.js
@@ -3,5 +3,13 @@
  * @param n {Integer} Number of levels deep from Fibonacci sequence.
  * @returns {Integer} Number from the Fibonacci sequence.
  */
-function fibo(n) {
+function fib(n) {
 }
+
+// Go over first 10 elements of the Fibonacci sequence.
+for (var i = 0; i < 10; i++) {
+  console.log(fib(i));
+}
+
+// Should log:
+// 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
