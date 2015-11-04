4977fe478ee08e53c6c0a9e9175d409c995c4e3e
e3 <eerwitt@gmail.com>
2015-11-04 14:28:58 -0800

    Initial JS file for help.

---
+++ fib.js
@@ -0,0 +1,7 @@
+/*
+ * Get the nth level deep number from a Fibonacci sequence.
+ * @param n {Integer} Number of levels deep from Fibonacci sequence.
+ * @returns {Integer} Number from the Fibonacci sequence.
+ */
+function fibo(n) {
+}
