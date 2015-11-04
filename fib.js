2ab2c48dbac2a5028d0cc3fcccdb7ed39a093fe4
e3 <eerwitt@gmail.com>
2015-11-04 14:37:56 -0800

    Fix problem in logic, I hope this is correct.

--- fib.js
+++ fib.js
@@ -3,5 +3,27 @@
  * @param n {Integer} Number of levels deep from Fibonacci sequence.
  * @returns {Integer} Number from the Fibonacci sequence.
  */
-function fibo(n) {
+function fib(n) {
+  var currentNum = 1;
+  var prevNum = 1;
+  var prevPrevNum = 0;
+
+  for (var i = 1; i <= n; i += 1) {
+    if (i > 1) {
+      prevPrevNum = prevNum;
+      prevNum = currentNum;
+      currentNum = prevNum + prevPrevNum;
+    }
+  }
+
+  return currentNum; 
 }
+
+
+// Go over first 10 elements of the Fibonacci sequence.
+for (var i = 0; i < 10; i++) {
+  console.log(fib(i));
+}
+
+// Should log:
+// 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
